ce81abed0b91d16ab706109c47067b350285c3d6
Dmitriy Angve <kirirch129@gmail.com>
2023-01-24 13:37:53 +0300

    #

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const url = "quiz.json";
 const questions = []; // load all of the data into the questions array
 const output = document.querySelector(".output");
 const btn = document.querySelector(".btn");
+let holder = []; // hold all of the newly constructed elements
 
 btn.addEventListener("click", (e) => {
   newQuestion();
@@ -16,26 +17,63 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 function newQuestion() {
+  const el = questions[cur]; // element object of the question
+  el.options.sort(() => {
+    return 0.5 - Math.random();
+  });
+
   console.log(cur);
   console.log(questions.length);
   console.log(questions[cur]);
 
   output.innerHTML = "";
-  const el = questions[cur];
+
   const que1 = document.createElement("div");
+  que1.classList.add("que");
+
+  let strOutput = el.question;
+  strOutput = strOutput.charAt(0).toUpperCase() + strOutput.slice(1);
+
   const ans1 = document.createElement("div");
-  que1.textContent = el.question;
+  que1.textContent = strOutput + "?";
+
+  holder.length = 0; // clear array
+
   el.options.forEach((ans) => {
     const div = document.createElement("div");
+
+    holder.push(div);
+
     div.textContent = ans.response;
-    div.addEventListener("click", (e) => {
-      console.log(ans.correct);
-    });
+    div.classList.add("box");
+    div.correct = ans.correct;
+    div.addEventListener("click", selOption);
     ans1.append(div);
   });
 
   output.append(que1);
   output.append(ans1);
+}
+
+// Function for remove event listener (to handle click event)
+function selOption(e) {
+  endTurn();
+
+  if (e.target.correct) {
+    e.target.style.backgroundColor = "green";
+  } else {
+    e.target.style.backgroundColor = "red";
+  }
+  e.target.style.color = "white";
+}
+
+function endTurn() {
+  holder.forEach((el) => {
+    el.removeEventListener("click", selOption);
+    el.style.backgroundColor = "#ddd";
+  });
+  btn.style.display = "block";
+  btn.textContent = "Next Question";
 }
 
 function loadQuestions() {
