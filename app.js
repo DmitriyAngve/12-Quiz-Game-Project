f3c8b52fa027a8f5f2db7a9dd0efbab2f30bda0a
Dmitriy Angve <kirirch129@gmail.com>
2023-01-24 14:22:05 +0300

    #

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const url = "quiz.json";
 const questions = []; // load all of the data into the questions array
 const output = document.querySelector(".output");
 const btn = document.querySelector(".btn");
+const totalOutput = document.querySelector("h1");
 let holder = []; // hold all of the newly constructed elements
+let cur = 0; // current question
+const player = { score: 0 };
 
 btn.addEventListener("click", (e) => {
-  newQuestion();
+  if (cur >= questions.length) {
+    output.innerHTML = "Game Over!";
+  } else {
+    newQuestion();
+  }
   btn.style.display = "none";
 });
-
-let cur = 0; // current question
 
 window.addEventListener("DOMContentLoaded", () => {
   //   console.log("ready");
@@ -17,6 +22,7 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 function newQuestion() {
+  updateScore();
   const el = questions[cur]; // element object of the question
   el.options.sort(() => {
     return 0.5 - Math.random();
@@ -46,6 +52,7 @@ function newQuestion() {
 
     div.textContent = ans.response;
     div.classList.add("box");
+    div.classList.add("boxCursor");
     div.correct = ans.correct;
     div.addEventListener("click", selOption);
     ans1.append(div);
@@ -58,22 +65,27 @@ function newQuestion() {
 // Function for remove event listener (to handle click event)
 function selOption(e) {
   endTurn();
-
   if (e.target.correct) {
+    player.score++;
+    updateScore();
     e.target.style.backgroundColor = "green";
   } else {
     e.target.style.backgroundColor = "red";
   }
   e.target.style.color = "white";
+  nextBtn();
+}
+
+function updateScore() {
+  totalOutput.innerHTML = `${cur} out of ${questions.length} Score: ${player.score}`;
 }
 
 function endTurn() {
   holder.forEach((el) => {
     el.removeEventListener("click", selOption);
     el.style.backgroundColor = "#ddd";
+    el.classList.remove("boxCursor");
   });
-  btn.style.display = "block";
-  btn.textContent = "Next Question";
 }
 
 function loadQuestions() {
@@ -99,12 +111,20 @@ function loadQuestions() {
         }; // obj for holding information and set it up as an object
         temp.push(tempObj);
         console.log(temp);
-
         let mainTemp = { question: el.question, options: temp };
-
         questions.push(mainTemp); // Populate an array
       });
       console.log(questions);
       //   document.write(JSON.stringify(questions));
     });
+}
+
+function nextBtn() {
+  btn.style.display = "block";
+  cur++;
+  if (cur >= questions.length) {
+    btn.textContent = "See Score";
+  } else {
+    btn.textContent = "Next Question";
+  }
 }
